e11a2253d0a3eeb930bc31d8db93c5acbf1393d3
PaleVerge <fnme370679@gmail.com>
2026-01-25 23:38:14 +0800

    第一次提交

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="app">
+    <h1 style="color: black;">Hello, Vue!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App"
+}
+</script>
+
+<style>
+.app {
+  box-shadow: inset 0 0 10px #000;
+  background-color: aquamarine;
+  padding: 10px;
+  border-radius: 10px;
+}
+</style>
