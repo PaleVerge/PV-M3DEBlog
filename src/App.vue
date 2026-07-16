d66422d431e5c80e5b3db8ac74ada871a6363277
PaleVerge <fnme370679@gmail.com>
2026-07-16 16:42:40 +0800

    修改小问题

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,29 @@
 <template>
-	<var-app-bar title="PaleVerge 's Blog" title-position="center" />
-	<md-outlined-button>Back</md-outlined-button>
-	<md-filled-button>Complete</md-filled-button>
-	<Person />
+  <div>
+    <h1>Welcome to PV's M3DEBlog!</h1>
+    <br />
+  </div>
+
+  <var-app-bar title="PaleVerge 's Blog" title-position="center" />
+
+  <div>
+    <br />
+    <Person />
+  </div>
+
+  <div>
+    <md-outlined-button>Back</md-outlined-button>
+    <md-filled-button>Complete</md-filled-button>
+  </div>
 </template>
 
 <script lang="ts">
 import Person from './components/Person.vue'
 
 export default {
-	name: "App",
-	components: { Person }
+  name: 'App',
+  components: { Person },
 }
-
 </script>
-
 
 <style></style>
